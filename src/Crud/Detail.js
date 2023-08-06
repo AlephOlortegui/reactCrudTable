143b5a390f383b9d7bdf61129be0803ad4e7c378
NeoDev <you@example.com>
2023-08-06 15:30:20 -0500

    Detail.js handleDelete

--- src/Crud/Detail.js
+++ src/Crud/Detail.js
@@ -1,11 +1,21 @@
 import useFetch from "../Custom/useFetch"
 import Spinner from "../Utilities/Spinner"
 import NotFound from "../Utilities/NotFound"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 const Detail = () => {
   const {id} = useParams()
   const {data: user, isPending, error} = useFetch(`http://localhost:8000/employee/${id}`)
+
+  //After deleting I want to redirect
+  const navigateTo = useNavigate()
+
+  const handleDelete = () => { 
+    fetch(`http://localhost:8000/employee/${id}`, {method: 'DELETE'})
+    .then(() => {console.log('deleted!'); navigateTo('/');})
+  }
+
+
   return (
     <>
     {isPending ? (<Spinner />) : error ? (<NotFound />) : (
@@ -15,7 +25,7 @@ const Detail = () => {
             <p className="card-text">The Employee name is : <b>{user.username}</b></p>
             <p className="card-text">Email is : {user.email}</p>
             <p className="card-text">Phone is : {user.phone}</p>
-            <button className="btn btn-danger me-3"><i className="bi bi-trash-fill"></i></button>
+            <button className="btn btn-danger me-3" onClick={handleDelete}><i className="bi bi-trash-fill"></i></button>
             <button className="btn btn-info ms-3"><i className="bi bi-pencil-fill"></i></button>
         </div>
       </div>
